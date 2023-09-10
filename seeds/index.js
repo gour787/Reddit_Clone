4867fd670dce97825cc43e0b21bf86f41ba2ea3e
rahulGour <rahul.om.gour@gmail.com>
2023-09-10 09:17:51 -0400

    added seed file for sample data

---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Post = require('../models/post');
+
+mongoose.connect('mongodb://127.0.0.1:27017/reddit-clone', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+
+
+const seedDB = async () => {
+    // Remove any existing entries in the "posts" collection
+    await Post.deleteMany({});
+    
+    for (let i = 0; i < 5; i++) {
+        const post = new Post({
+            title: `Title ${i + 1}`,
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            upvote: Math.floor(Math.random() * 100), // random number
+            downvote: Math.floor(Math.random() * 100) // random number
+        });
+
+        await post.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
+
+seedDB();
