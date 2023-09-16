2b360727cafa5204db0c0293f12b1308fd441d05
rahulGour <rahul.om.gour@gmail.com>
2023-09-16 15:27:23 -0400

    added popular page

--- routes/posts.js
+++ routes/posts.js
@@ -28,6 +28,20 @@ router.post('/', isLoggedIn, validatePost, catchAsync(async (req, res, next) => 
     res.redirect(`/posts/${post._id}`)
 }))
 
+// Define a route to display popular posts
+
+router.get('/popular', async (req, res) => {
+    try {
+      // Sort posts by upvote count in descending order and populate the 'author' field
+      const posts = await Post.find().sort({ upvote: 'desc' }).populate('author');
+      res.render('posts/popular', { posts }); // Replace 'popular' with the actual path to your popular.ejs file
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error'); // Handle errors gracefully
+    }
+  });
+  
+
 router.get('/:id', catchAsync(async (req, res,) => {
     const post = await Post.findById(req.params.id).populate({
         path: 'comments',
@@ -41,6 +55,8 @@ router.get('/:id', catchAsync(async (req, res,) => {
     }
     res.render('posts/show', { post });
 }));
+
+
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const post = await Post.findById(req.params.id)
@@ -58,20 +74,42 @@ router.put('/:id', isLoggedIn, isAuthor, validatePost, catchAsync(async (req, re
     return res.redirect(`/posts/${post._id}`)
 }));
 
-router.put('/:id/vote', catchAsync(async (req, res) => {
+router.put('/:id/upvote', catchAsync(async (req, res) => {
     const { id } = req.params;
-    const { upvote, downvote } = req.body;
-    console.log(upvote);
-
+    const { upvote } = req.body;
+  
     // Parse values as integers
-    const parsedUpvote = parseInt(upvote) +1;
-
+    const parsedUpvote = parseInt(upvote) + 1;
+  
     // Use findByIdAndUpdate to update the values in the database
-    await Post.findByIdAndUpdate(id, { upvote: parsedUpvote});
-
-    return res.redirect(`/posts`)
-}));
-
+    await Post.findByIdAndUpdate(id, { upvote: parsedUpvote });
+  
+    // Check if the URL does not contain '/popular'
+    if (!req.originalUrl.includes('/popular')) {
+      return res.redirect('/posts');
+    } else {
+      return res.redirect('/posts/popular');
+    }
+  }));
+  
+  router.put('/:id/downvote', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const { downvote } = req.body;
+  
+    // Parse values as integers
+    const parsedDownvote = parseInt(downvote) + 1;
+  
+    // Use findByIdAndUpdate to update the values in the database
+    await Post.findByIdAndUpdate(id, { downvote: parsedDownvote });
+  
+    // Check if the URL does not contain '/popular'
+    if (!req.originalUrl.includes('/popular')) {
+      return res.redirect('/posts');
+    } else {
+      return res.redirect('/posts/popular');
+    }
+  }));
+  
 
 
 
@@ -81,6 +119,15 @@ router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     req.flash('success', 'Successfully deleted post')
     res.redirect('/posts');
 }));
+
+
+
+  
+
+  
+  
+
+  
 
 
 module.exports = router;
